d1529e62162bc4904001b23f403898f0d9d6187b
jvbsilva <joaovictorbarros1@gmail.com>
2024-09-04 13:14:54 -0300

    Refactoring

--- create_chord_progressions.py
+++ create_chord_progressions.py
@@ -1,42 +1,88 @@
+import os
 from mido import Message, MidiFile, MidiTrack
 
 # Mapping of chords intervals in the key of C major
-chords = {
-    'i':   [60, 63, 67], # C minor chord: C4, Eb4, G4
-    'I':   [60, 64, 67], # C major chord: C4, E4, G4
-    'ii':  [62, 65, 69], # D minor chord: D4, F4, A4
-    'II':  [62, 66, 69], # D major chord: D4, F#4, A4
-    'iii': [64, 67, 71], # E minor chord: E4, G4, B4
-    'III': [64, 68, 71], # E major chord: E4, G#4, B4
-    'iv':  [65, 69, 72], # F minor chord: F4, A4, C5
-    'IV':  [65, 69, 72], # F major chord: F4, A4, C5
-    'v':   [67, 70, 74], # G minor chord: G4, Bb4, D5
-    'V':   [67, 71, 74], # G major chord: G4, B4, D5
-    'vi':  [69, 72, 76], # A minor chord: A4, C5, E5
-    'VI':  [69, 73, 76], # A major chord: A4, C#5, E5
-    'vii': [71, 74, 77], # B diminished chord: B4, D5, F5
-    'VII': [71, 75, 78]  # B major chord: B4, D#5, F#5
+CHORDS = {
+    "i": [60, 63, 67],  # C minor chord: C4, Eb4, G4
+    "I": [60, 64, 67],  # C major chord: C4, E4, G4
+    "ii": [62, 65, 69],  # D minor chord: D4, F4, A4
+    "II": [62, 66, 69],  # D major chord: D4, F#4, A4
+    "iii": [64, 67, 71],  # E minor chord: E4, G4, B4
+    "III": [64, 68, 71],  # E major chord: E4, G#4, B4
+    "iv": [65, 68, 72],  # F minor chord: F4, A4, C5
+    "IV": [65, 69, 72],  # F major chord: F4, A4, C5
+    "v": [67, 70, 74],  # G minor chord: G4, Bb4, D5
+    "V": [67, 71, 74],  # G major chord: G4, B4, D5
+    "vi": [69, 72, 76],  # A minor chord: A4, C5, E5
+    "VI": [69, 73, 76],  # A major chord: A4, C#5, E5
+    "vii": [71, 74, 78],  # B diminished chord: B4, D5, F5
+    "VII": [71, 75, 78],  # B major chord: B4, D#5, F#5
 }
 
+PROGRESSIONS = [
+    # Minor key
+    ["i", "VI", "VII"],
+    ["i", "iv", "VII"],
+    ["i", "iv", "v"],
+    ["i", "VI", "III", "VII"],
+    ["ii", "v", "i"],
+    ["i", "iv", "v", "i"],
+    ["VI", "VII", "i", "i"],
+    ["i", "VII", "VI", "VII"],
+    ["i", "iv", "i"],
+    # Major key
+    ["I", "IV", "V"],
+    ["I", "vi", "IV", "V"],
+    ["II", "V", "I"],
+    ["I", "vi", "ii", "V"],
+    ["I", "V", "vi", "IV"],
+    ["I", "IV", "vi", "V"],
+    ["I", "iii", "IV", "V"],
+    ["I", "IV", "I", "V"],
+    ["I", "IV", "ii", "V"],
+]
 
-def generate_chord_progression(progression, filename, velocity=64, duration=480):
+
+def generate_chord_progression(progression, velocity=64, duration=960):
+
+    for chord_symbol in progression:
+        chord = CHORDS.get(chord_symbol)
+        if not chord:
+            raise ValueError(f"Invalid chord symbol: {chord_symbol}")
+
+    folder_name = "chord_progressions"
+
+    if progression[0].isupper():
+        filename = f"major_{'-'.join(progression)}.mid"
+    else:
+        filename = f"minor_{'-'.join(progression)}.mid"
+
+    file_path = os.path.join(folder_name, filename)
+
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
     mid = MidiFile()
     track = MidiTrack()
     mid.tracks.append(track)
 
     for chord_symbol in progression:
-        chord = chords.get(chord_symbol)
-        if not chord:
-            raise ValueError(f"Invalid chord symbol: {chord_symbol}")
-        
+        chord = CHORDS.get(chord_symbol)
+
+        # Notes on
         for note in chord:
-            track.append(Message('note_on', note=note, velocity=velocity, time=0))
-        track.append(Message('note_off', note=chord[0], velocity=velocity, time=duration))
-        
+            track.append(Message("note_on", note=note, velocity=velocity, time=0))
+
+        # Notes off
+        track.append(
+            Message("note_off", note=chord[0], velocity=velocity, time=duration)
+        )
         for note in chord[1:]:
-            track.append(Message('note_off', note=note, velocity=velocity, time=0))
+            track.append(Message("note_off", note=note, velocity=velocity, time=0))
 
-    mid.save(filename)
+    mid.save(file_path)
 
-# Example usage: Generate a I-IV-V-I progression in C major
-generate_chord_progression(['I', 'IV', 'V', 'I'], 'chord_progression.mid')
+
+if __name__ == "__main__":
+    for progression in PROGRESSIONS:
+        generate_chord_progression(progression)
--- create_note_intervals.py
+++ create_note_intervals.py
@@ -1,53 +1,62 @@
 from mido import Message, MidiFile, MidiTrack
 import os
+
 # Mapping of interval names to semitones
 intervals = {
-    'minor_second': 1,
-    'major_second': 2,
-    'minor_third': 3,
-    'major_third': 4,
-    'perfect_fourth': 5,
-    'tritone': 6,
-    'perfect_fifth': 7,
-    'minor_sixth': 8,
-    'major_sixth': 9,
-    'minor_seventh': 10,
-    'major_seventh': 11,
-    'octave': 12
+    "minor_second": 1,
+    "major_second": 2,
+    "minor_third": 3,
+    "major_third": 4,
+    "perfect_fourth": 5,
+    "tritone": 6,
+    "perfect_fifth": 7,
+    "minor_sixth": 8,
+    "major_sixth": 9,
+    "minor_seventh": 10,
+    "major_seventh": 11,
+    "octave": 12,
 }
 
-def generate_midi(note: int, interval_name: str):
-    mid = MidiFile()
-    track = MidiTrack()
-    mid.tracks.append(track)
 
-    # Start with the root note
-    track.append(Message('note_on', note=note, velocity=64, time=0))
-    track.append(Message('note_off', note=note, velocity=64, time=480))
+def generate_midi(note: int, interval_name: str, increase: bool = True):
 
     # Calculate the next note based on the interval
     interval = intervals.get(interval_name.lower())
     if interval is None:
         raise ValueError(f"Invalid interval name: {interval_name}")
 
-    next_note = note + interval
-
-    # Add the next note
-    track.append(Message('note_on', note=next_note, velocity=64, time=480))
-    track.append(Message('note_off', note=next_note, velocity=64, time=480))
-
-    # Save the MIDI file
     folder_name = "note_intervals"
+
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
-    filename = f"{note}_{interval_name}.mid"
+
+    if increase:
+        next_note = note + interval
+        filename = f"{note}_{interval_name}_up.mid"
+    else:
+        next_note = note - interval
+        filename = f"{note}_{interval_name}_down.mid"
+
     file_path = os.path.join(folder_name, filename)
+
+    mid = MidiFile()
+    track = MidiTrack()
+    mid.tracks.append(track)
+
+    # Start with the root note
+    track.append(Message("note_on", note=note, velocity=64, time=0))
+    track.append(Message("note_off", note=note, velocity=64, time=480))
+
+    # Add the next note
+    track.append(Message("note_on", note=next_note, velocity=64, time=480))
+    track.append(Message("note_off", note=next_note, velocity=64, time=480))
+
+    # Save the MIDI file
     mid.save(file_path)
 
-def main():
-    for interval in intervals:
-        for note in range(60,72,1):
-            generate_midi(note, interval)
-            
+
 if __name__ == "__main__":
-    main()
+    for interval in intervals:
+        for note in range(60, 72, 1):
+            generate_midi(note, interval)
+            generate_midi(note, interval, False)
