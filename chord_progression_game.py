c7b7fed1921310d0a3c0196a108b1fb1bda752cb
João Victor Barros da Silva <joaovictorbarros1@gmail.com>
2024-09-05 14:28:15 -0300

    Adding feature to play user input

--- chord_progression_game.py
+++ chord_progression_game.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import os
+from create_chord_progressions import is_valid_progression,create_mid_from_progression
 
 FORLDER_NAME = "chord_progressions"
 FILE_LIST = [f for f in os.listdir(FORLDER_NAME) if f.endswith(".mid")]
@@ -15,41 +16,64 @@ def get_song():
 
 if __name__ == "__main__":
     pygame.init()
+    
     file_name, file_path, progression_name = get_song()
     pygame.mixer.music.load(file_path)
+    pygame.mixer.music.play()
 
-    right_answer = False
-    user_input = "R"
     print("***************************************************")
     print("* Welcome to the chord progression guessing game! *")
     print("***************************************************")
     print("Press 'Q' to exit the game.")
     print("Press 'R' to listen again.")
-    while user_input.upper() == "R":
-        print(progression_name)
-        pygame.mixer.music.play()
+    
+    right_answer = False
+    user_input = ""
+
+    print(progression_name)
+    while True:
+        
         user_input = input("Enter your answer: ")
+
         if user_input.upper() == "Q":
             pygame.quit()
             break
+
+        if user_input.upper() == "R":
+            pygame.mixer.music.load(file_path)
+            pygame.mixer.music.play()
+            continue
+        
         if user_input == progression_name:
             right_answer = True
             print("RIGHT ANSWER!!")
             user_input = input("Keep playing? [Y/N]: ")
+            
             if user_input.upper() == "Y":
-                user_input = "R"
                 file_name, file_path, progression_name = get_song()
-                print(progression_name)
                 pygame.mixer.music.load(file_path)
+                pygame.mixer.music.play()
                 continue
             pygame.quit()
             break
-        elif user_input.upper() != "R":
-            print("WRONG ANSWER!!")
-            user_input = input("Keep trying? [Y/N]: ")
-            if user_input.upper() == "Y":
-                user_input = "R"
+        
+        user_progression = user_input.split(" ") 
+        if is_valid_progression(user_progression):
+            user_progression_mid = create_mid_from_progression(user_progression)
+            user_progression_mid.save("user_progression.mid")
+            pygame.mixer.music.load("user_progression.mid")
+            pygame.mixer.music.play()
+            while pygame.mixer.music.get_busy():
+                pygame.time.wait(1000)
+ 
+        print("WRONG ANSWER!!")
+        user_input = input("Keep trying? [Y/N]: ")
+        if user_input.upper() == "Y":
+            user_input = "R"
+    
     if not right_answer:
         print(f"Right answer was: {progression_name}")
 
     pygame.quit()
+    if os.path.exists("user_progression.mid"):
+        os.remove("user_progression.mid")
