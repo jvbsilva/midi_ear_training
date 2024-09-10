2cfcf3f489fb1131fc9fb935b710348e1629810b
João Victor Barros da Silva <joaovictorbarros1@gmail.com>
2024-09-10 13:20:53 -0300

    creating target progression when running

--- chord_progression_game.py
+++ chord_progression_game.py
@@ -1,22 +1,23 @@
 import pygame
 import random
 import os
-from create_chord_progressions import is_valid_progression, create_mid_from_progression
+from create_chord_progressions import is_valid_progression, create_mid_from_progression, save_chord_progression
+from create_chord_progressions import CHORDS, PROGRESSIONS
 
-FORLDER_NAME = "chord_progressions"
-FILE_LIST = [f for f in os.listdir(FORLDER_NAME) if f.endswith(".mid")]
-TEMP_FILE = "user_progression.mid"
-
-
-def get_song():
-    file_name = random.choice(FILE_LIST)
-    file_path = os.path.join(FORLDER_NAME, file_name)
-    progression_name = file_name.split(".")[0].split("_")[1].replace("-", " ")
-    return file_name, file_path, progression_name
+TARGET_PROG_FILE = "target_progression.mid"
+USER_PROG_FILE = "user_progression.mid"
 
 
-def play_song(file_path):
-    pygame.mixer.music.load(file_path)
+def select_target_progression():
+    target_progresison = random.choice(PROGRESSIONS)
+    mid = create_mid_from_progression(target_progresison)
+    mid.save(TARGET_PROG_FILE)
+    target_progression_name = " ".join(target_progresison)
+    return target_progression_name
+
+
+def play_mid(mid_file:str):
+    pygame.mixer.music.load(mid_file)
     pygame.mixer.music.play()
     while pygame.mixer.music.get_busy():
         pygame.time.wait(1000)
@@ -32,8 +33,8 @@ if __name__ == "__main__":
 
     pygame.init()
 
-    file_name, file_path, progression_name = get_song()
-    play_song(file_path)
+    target_progression_name = select_target_progression()
+    play_mid(TARGET_PROG_FILE)
 
     right_answer = False
     user_input = ""
@@ -43,22 +44,24 @@ if __name__ == "__main__":
         user_input = input("Enter your answer: ")
 
         if user_input.upper() == "Q":
+            print(f"Right answer was: {target_progression_name}")
             pygame.quit()
             break
 
         if user_input.upper() == "R":
-            pygame.mixer.music.load(file_path)
+            
+            pygame.mixer.music.load(TARGET_PROG_FILE)
             pygame.mixer.music.play()
             continue
 
-        if user_input == progression_name:
+        if user_input == target_progression_name:
             right_answer = True
             print("RIGHT ANSWER!!")
             user_input = input("Keep playing? [Y/N]: ")
 
             if user_input.upper() == "Y":
-                file_name, file_path, progression_name = get_song()
-                play_song(file_path)
+                target_progression_name = select_target_progression()
+                play_mid(TARGET_PROG_FILE)
                 continue
             pygame.quit()
             break
@@ -66,14 +69,13 @@ if __name__ == "__main__":
         user_progression = user_input.strip().split()
         if is_valid_progression(user_progression, debug=False):
             user_progression_mid = create_mid_from_progression(user_progression)
-            user_progression_mid.save(TEMP_FILE)
-            play_song(TEMP_FILE)
+            user_progression_mid.save(USER_PROG_FILE)
+            play_mid(USER_PROG_FILE)
 
         print("WRONG ANSWER!!")
 
-    if not right_answer:
-        print(f"Right answer was: {progression_name}")
-
     pygame.quit()
-    if os.path.exists(TEMP_FILE):
-        os.remove(TEMP_FILE)
+    if os.path.exists(USER_PROG_FILE):
+        os.remove(USER_PROG_FILE)
+    if os.path.exists(TARGET_PROG_FILE):
+        os.remove(TARGET_PROG_FILE)
